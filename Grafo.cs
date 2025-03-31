f8bd45b2076533f20595914023c9e0ae9447e6dd
Alejandro Guzman <alejandroguzca@gmail.com>
2025-03-31 17:42:32 -0600

    Arreglar Busqueda BFS

---
+++ Grafo.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicación_de_grafos
+{
+    internal class Grafo
+    {
+        public int Vertix;
+        public Dictionary<int, List<int>> AdjList;
+
+        public Grafo()
+        {
+            AdjList = new Dictionary<int, List<int>>();
+        }
+
+        public void InsertVertix(int Vertix)
+        {
+            AdjList[Vertix] = new List<int>();
+        }
+
+        public void InsertEdge(int VertixA, int VertixB, bool IsDirected)
+        {
+            if (AdjList.ContainsKey(VertixA))
+            {
+                AdjList[VertixA].Add(VertixB);
+                if (!IsDirected && AdjList.ContainsKey(VertixB))
+                {
+                    AdjList[VertixB].Add(VertixA);
+                }
+            }
+        }
+
+        public void MostrarListaAdy()
+        {
+            int i = 0;
+            foreach (var ParVertix in AdjList)
+            {
+                Console.Write(ParVertix.Key + " ---> ");
+                foreach (var item in AdjList[ParVertix.Key])
+                {
+                        Console.Write(item + " ");
+                    i++;
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void DFS(int origin)
+        {
+            Dictionary<int, bool> visited = new Dictionary<int, bool>();
+            foreach (var ParVertix in AdjList)
+            {
+                visited[ParVertix.Key] = false;
+            }
+            DFS(origin, visited);
+        }
+        public void DFS(int startVertix, Dictionary<int, bool> visited)
+        {
+            if (visited[startVertix]) { return; } 
+
+            Console.Write(startVertix + " ");
+            visited[startVertix] = true; 
+
+            foreach (int neighbor in AdjList[startVertix])
+            {
+                DFS(neighbor, visited);
+            }
+        }
+
+        public void BFS(int origin)
+        {
+            Dictionary<int, bool> visited = new Dictionary<int, bool>();
+            foreach (var ParVertix in AdjList)
+            {
+                visited[ParVertix.Key] = false;
+            }
+
+            Queue<int> queue = new Queue<int>();
+            
+           queue.Enqueue(origin);
+           while (queue.Any())
+            {
+                int current = queue.Dequeue();
+                Console.Write(current + " ");
+                foreach(int i in AdjList[current])
+                {
+                    {
+                        if (!visited[i])
+                        {
+                            queue.Enqueue((int)i);
+                            visited[i] = true;
+                        }
+                    }
+                }
+                
+            }
+        
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,92 @@ namespace Aplicación_de_grafos
     {
         static void Main(string[] args)
         {
+            Grafo grafo = new Grafo();
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("\n--- Menú ---");
+                Console.WriteLine("1. Insertar vértice");
+                Console.WriteLine("2. Insertar arista");
+                Console.WriteLine("3. Mostrar lista de adyacencia");
+                Console.WriteLine("4. Recorrido en DFS");
+                Console.WriteLine("5. Recorrido en BFS");
+                Console.WriteLine("6. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                if (!int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    Console.WriteLine("Opción no válida, intente de nuevo.");
+                    continue;
+                }
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Write("Ingrese el vértice a agregar: ");
+                        if (int.TryParse(Console.ReadLine(), out int vertice))
+                        {
+                            grafo.InsertVertix(vertice);
+                            Console.WriteLine("Vértice agregado.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Entrada no válida.");
+                        }
+                        break;
+
+                    case 2:
+                        Console.Write("Ingrese el vértice origen: ");
+                        if (!int.TryParse(Console.ReadLine(), out int vertA)) break;
+                        Console.Write("Ingrese el vértice destino: ");
+                        if (!int.TryParse(Console.ReadLine(), out int vertB)) break;
+                        Console.Write("Es dirigido? (1: Sí, 0: No): ");
+                        bool isDirected = Console.ReadLine() == "1";
+                        grafo.InsertEdge(vertA, vertB, isDirected);
+                        Console.WriteLine("Arista agregada.");
+                        break;
+
+                    case 3:
+                        grafo.MostrarListaAdy();
+                        break;
+
+                    case 4:
+                        Console.Write("Ingrese el vértice de inicio para DFS: ");
+                        if (int.TryParse(Console.ReadLine(), out int startDFS))
+                        {
+                            grafo.DFS(startDFS);
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Entrada no válida.");
+                        }
+                        break;
+
+                    case 5:
+                        Console.Write("Ingrese el vértice de inicio para BFS: ");
+                        if (int.TryParse(Console.ReadLine(), out int startBFS))
+                        {
+                            grafo.BFS(startBFS);
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Entrada no válida.");
+                        }
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Saliendo...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+
+            } while (opcion != 6);
         }
     }
 }
